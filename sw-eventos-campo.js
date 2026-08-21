889cb2de9dd0b0460d59784e9b71e2c21b0a71d7
GiuseppeMarques <www.gfm@gmail.com>
2026-08-21 07:33:05 -0300

    Add files via upload

--- sw-eventos-campo.js
+++ sw-eventos-campo.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eventos-campo-v9';
+const CACHE_NAME = 'eventos-campo-v12';
 
 self.addEventListener('install', function(e){
   self.skipWaiting();
--- sw-eventos-quartel.js
+++ sw-eventos-quartel.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eventos-quartel-v12';
+const CACHE_NAME = 'eventos-quartel-v18';
 
 self.addEventListener('install', function(e){
   self.skipWaiting();
